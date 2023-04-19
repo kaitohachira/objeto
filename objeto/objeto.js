201e9f61b329d16842f30472c98302b76fa41a82
Ally <110118395+kaitohachira@users.noreply.github.com>
2023-04-19 17:53:12 -0300

    Add files via upload

---
+++ objeto/objeto.js
@@ -0,0 +1,20 @@
+//Código não executável
+
+//procedural
+processamento(valor1, valor2, valor3)
+
+//Orientação ao objeto 
+objeto = {
+   valor1, 
+   valor2,
+   valor3,
+   processamento(){}
+}
+
+objeto = ['valor1', 'valor2','valor3']
+
+//Principios da orientação ao objeto
+//1 - abstração
+//2 - encapsulamento
+//3 - herança(prototype)
+//4 - polimorfismo
